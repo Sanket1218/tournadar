7b25c727fac901e1e54183c0b6fc3fb81f72d70d
Sanket1218 <iamsanketyadav5@gmail.com>
2025-08-08 03:09:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
 
 
 
